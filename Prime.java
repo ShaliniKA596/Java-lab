45c67a32b1f4332b0e1c8ff71e567be26bc0f801
ShaliniKA596 <shalinika.cs25@bmsce.ac.in>
2025-09-28 11:41:33 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,24 @@
+public class Prime {
+    public static void main(String[] args) {
+        int number = 30; // You can change this value
+
+        boolean isPrime = true;
+
+        if (number < 2) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
